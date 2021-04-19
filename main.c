d586fe132a4e082aa8acb60fc31bc92f60d776e8
Your Name <you@example.com>
2021-04-19 10:01:06 +0200

    created a student structure

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct StudentDATA {
+    char* name;
+    int ID;
+    int day,month,year;
+    int scorelastyear;
+};
+
+int main()
+{int n;
+    printf("On behalf of all of us, welcome onboard\n");
+   printf("Enter the number of student\n");
+   scanf("%d",&n) ;
+
+    return 0;
+}
