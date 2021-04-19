50f3a08d5a8d5c18237dcada46e5a00d18ce2a01
menna abdelkader <mennabdlkader102@gmail.com>
2021-04-19 11:09:43 +0200

    editing

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct StudentDATA {
+struct Student{
     char* name;
     int ID;
     int day,month,year;
     int scorelastyear;
 };
+void studentdata ()
+{
+    for (int i=0;i<=n ;i++)
+    {printf("Enter the  name of student\n");
+
+    }
+}
 
 int main()
 {int n;
-    printf("On behalf of all of us, welcome onboard\n");
+    printf("On behalf of all of us, welcome on board\n");
    printf("Enter the number of student\n");
    scanf("%d",&n) ;
 
